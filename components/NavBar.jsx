f8e9ac7fec9448a2789c37b18d817a692c023893
Scott Parrett <sparrett@usa.net>
2025-09-14 16:16:40 -0400

    fix navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 // import ScrollLink from '../components/Scrolls/ScrollLink';
 import { NavLink } from 'react-router-dom';
-import "../styles/components/NavBar.css";
+import "../styles/components/navbar.css";
 
 
 function LinkPill({ to, children }) {
