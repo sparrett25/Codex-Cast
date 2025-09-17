6ae54376e64d780fa3794acdac987db789223a0a
Scott Parrett <sparrett@usa.net>
2025-09-16 23:59:31 -0400

    update assetMap

--- assets/assetMap.js
+++ assets/assetMap.js
@@ -1,13 +1,13 @@
 // src/cast/assets/assetMap.js
 const assets = {
   backgrounds: {
-    "bg_mirror_lake.jpg": "./assets/backgrounds/bg_mirror_lake.jpg",
-    "bg_baitshop.jpg": "./assets/backgrounds/bg_baitshop.jpg",
-	"bg_cabin_interior.svg": "./assets/backgrounds/bg_cabin_interior.jpg",
+    "bg_mirror_lake.jpg": "/assets/backgrounds/bg_mirror_lake.jpg",
+    "bg_baitshop.jpg": "/assets/backgrounds/bg_baitshop.jpg",
+	"bg_cabin_interior.svg": "/assets/backgrounds/bg_cabin_interior.jpg",
   },
   sprites: {
-    player: "./assets/sprites/player.png",
-    shopkeeper: "./assets/sprites/shopkeeper.png",
+    player: "/assets/sprites/player.png",
+    shopkeeper: "/assets/sprites/shopkeeper.png",
   },
   effects: {},
 };
