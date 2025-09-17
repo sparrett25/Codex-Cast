3e21b94898c65df22b6b6fd276b100fe3b55bf4e
Scott Parrett <sparrett@usa.net>
2025-09-17 00:33:56 -0400

    public assets fix

--- assets/assetMap.js
+++ assets/assetMap.js
@@ -1,25 +1,14 @@
 // src/cast/assets/assetMap.js
-
-
-import bgLake from "./backgrounds/bg_mirror_lake.jpg";
-import bgBait from "./backgrounds/bg_baitshop.jpg";
-import bgCabin from "./backgrounds/bg_cabin_interior.jpg"; 
-
-
-import spriteShopkeeper from "./sprites/shopkeeper.png";
-import spriteSwallow from "./sprites/player.png"; 
-
-
-
 const assets = {
   backgrounds: {
-    "bg_mirror_lake.jpg": bgLake,
-    "bg_baitshop.jpg": bgBait,
-    "bg_cabin_interior.svg": bgCabin,
+    "bg_mirror_lake.jpg": "/assets/backgrounds/bg_mirror_lake.jpg",
+    "bg_baitshop.jpg": "/assets/backgrounds/bg_baitshop.jpg",
+    "bg_cabin_interior.jpg": "/assets/backgrounds/bg_cabin_interior.jpg", // ← PNG/JPG
   },
   sprites: {
-    player: spritePlayer,
-    shopkeeper: spriteShopkeeper,
+    player: "/assets/sprites/player.png",
+    shopkeeper: "/assets/sprites/shopkeeper.png",
+    
   },
   effects: {},
 };
